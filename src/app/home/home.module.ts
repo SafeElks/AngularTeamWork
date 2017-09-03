39d1f7b35a557a4c16f35433e456eaac132de14c
Pernikoff <downingtn@abv.bg>
2017-09-03 20:19:14 +0300

    home page in progress(material inspired)

--- src/app/home/home.module.ts
+++ src/app/home/home.module.ts
@@ -1,14 +1,19 @@
+import { MdButtonModule } from '@angular/material';
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { HomeComponent } from './home.component';
 import { HomeRouterModule } from './home-router.module';
+import { HeaderComponent } from './header/header.component';
 
 @NgModule({
   imports: [
     CommonModule,
-    HomeRouterModule
+    HomeRouterModule,
+    MdButtonModule
   ],
-  declarations: [HomeComponent]
+  declarations: [HomeComponent,
+    HeaderComponent
+]
 })
 export class HomeModule { }
