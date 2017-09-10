edf8698a5fe9c35e694226620791760ae4f49ded
Pernikoff <downingtn@abv.bg>
2017-09-10 15:18:15 +0300

    responsive done(except /users), navicon added

--- src/app/home/header/header.component.ts
+++ src/app/home/header/header.component.ts
@@ -6,7 +6,7 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./header.component.css']
 })
 export class HeaderComponent implements OnInit {
-
+  title = 'FIT.me';
   coverImg = '../../../assets/images/cover.jpg';
 
   constructor() { }
