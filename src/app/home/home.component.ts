2f6a0d0da4e8ac64a04b85e122fe2a9dc4cad909
Pernikoff <downingtn@abv.bg>
2017-09-11 02:59:00 +0300

    title, plan and bfc on home page when isLoggedIn

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,13 +1,15 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, DoCheck } from '@angular/core';
 import { HomeService } from '../services/home.service';
+import { AuthService } from '../services/auth.service';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.css']
 })
-export class HomeComponent implements OnInit {
+export class HomeComponent implements OnInit, DoCheck {
   info: string;
+  public isLoggedIn: boolean;
   logos = [{
     logoUrl: '../../assets/images/healthy.png', text: 'Eat Healthy'
   }, {
@@ -17,14 +19,18 @@ export class HomeComponent implements OnInit {
   }];
 
   small = window.innerWidth < 425 ? true : false;
-  constructor(private appService: HomeService) { }
+  constructor(private appService: HomeService, private authService: AuthService) { }
 
   ngOnInit(): void {
+    this.isLoggedIn = this.authService.isLogged();
     this.appService.getHomeInfo()
       .map((res) => res.json())
       .subscribe((response: any) => {
         this.info = response.info;
       });
-      console.log(this.small);
+    console.log(this.small);
+  }
+  ngDoCheck() {
+    this.isLoggedIn = this.authService.isLogged();
   }
 }
