b1709dc07ebc7c81e0ba2fdf683f3802927802a2
Pernikoff <downingtn@abv.bg>
2017-09-11 03:14:20 +0300

    list done for now

--- src/app/users/list/list.component.ts
+++ src/app/users/list/list.component.ts
@@ -16,7 +16,7 @@ import {AuthService} from '../../services/auth.service';
 })
 
 export class ListComponent implements OnInit {
-  displayedColumns = ['photo', 'userName', 'gender', 'age', 'weight', 'height', 'dreamKg', 'email'];
+  displayedColumns = ['userName', 'email'];
   users: Array<any>;
   dataSource: UsersDataSource | null;
   isAuthenticated: boolean;
